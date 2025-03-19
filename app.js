4cad07837fa6263ec86703deb881de62786d3aca
Tonatiuh Durán <tonatiuhdf@hotmail.com>
2025-03-18 20:43:51 -0600

    Agregamos la función del botón Add/Añadir, el cual va a capturar y almacenar la respuesta del usuario

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let nombresAmigos = []
+
+function agregarAmigo() {
+
+    let amigos = document.getElementById("amigo").value;
+    console.log(amigos) 
+
+    if (amigos == "") {
+        alert("Por favor, ingresa un nombre");
+        return;
+    }
+
+    nombresAmigos.push(amigos);
+
+    document.getElementById("amigo").value = "";
+    
+    mostrarAmigos()
+}
